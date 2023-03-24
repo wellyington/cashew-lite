a67f1b26717e94557dae2812ab6bd3c92e3adb82
Wellington Ameida <64033017+wellyington@users.noreply.github.com>
2023-03-24 20:21:51 +0000

    instagram_hashtag_engagement()
    Incremented function instagram_hashtag_engagement()

--- firefox.py
+++ firefox.py
@@ -21,15 +21,54 @@ driver = webdriver.Firefox(options=options, service=Service(GeckoDriverManager()
 
 # Instagram Functions
 
+# Open Instagram
+
 def instagram():
 	driver.get("https://www.instagram.com/")
 	print(">>> Please Loging to Your Instagram Account!")
+
+# Instagram Engagement Function by Hashtag
 	
 def instagram_hashtag_engagement():
+    
+    # Set variable counter to 0 : Will be used to count engagements!
+    counter = 0
+
+    # Collection Hastag
     hashtag = input(">>> Select 1 Hashtag: #")
-    limit = input(">> How many posts would you like to interact with: ")
+    limit = input(">>> How many posts would you like to interact with: ")
+    
+    # Opening Felected hashtag
     driver.get("https://www.instagram.com/explore/tags/" + hashtag)
     timecounter(20)
+
+    # Opening First Post From The Hashtag Page
     driver.find_element(by=By.XPATH, value=xpaths.instagram_post).click()
-    timecounter(20)
-    driver.find_element(by=By.XPATH, value=xpaths.instagram_like).click()
+    timecounter(10)
+
+    # Interating Engagements 
+
+    while counter != int(limit):
+
+        # Liking post
+        driver.find_element(by=By.XPATH, value=xpaths.instagram_like).click()
+
+        timecounter(15)
+
+        # Moving to The Next Post
+        try: 
+            # Button to Move to The Next Post (All Posts but not First)
+            driver.find_element(by=By.XPATH, value=xpaths.instagram_next2).click()
+        except:
+            # Button to Move to The Second Post (First post Only)
+            # This button will will be clicked only once during execution
+            driver.find_element(by=By.XPATH, value=xpaths.instagram_next1).click()
+        
+        # Update Variable 'counter'
+        counter = counter + 1
+
+        print(f">>> Engaged: {counter}")
+
+        timecounter(5)
+
+    print(">>> Engagement Completed.")
--- xpaths.py
+++ xpaths.py
@@ -4,3 +4,5 @@
 
 instagram_post = "//html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div"
 instagram_like = "//html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button"
+instagram_next1 = "//html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div/button"
+instagram_next2 = "//html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div/div/div[2]/button"
